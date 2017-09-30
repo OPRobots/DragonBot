6ca9b54848e12af58bdbd1bed571240771e4841e
robotaleh <alex.santos.ete@gmail.com>
2017-09-30 19:49:26 +0200

    Añadir pines iniciales

--- src/Main.ino
+++ src/Main.ino
@@ -1,3 +1,44 @@
+///////////////////
+// PINES MOTORES //
+///////////////////
+#define MOTOR_IZQUIERDO_ADELANTE PB12
+#define MOTOR_IZQUIERDO_ATRAS PB13
+#define MOTOR_IZQUIERDO_PWM PB1
+#define MOTOR_DERECHO_ADELANTE PB14
+#define MOTOR_DERECHO_ATRAS PB15
+#define MOTOR_DERECHO_PWM PA8
+#define MOTOR_RUN PB10
+#define MOTOR_DERECHO_ENCODER PB11
+#define MOTOR_IZQUIERDO_ENCODER PB9
+#define MOTOR_VENTILADOR PB8
+
+//////////////////
+// PINES SWITCH //
+//////////////////
+#define SW_1 PA11
+#define SW_2 PA12
+
+//////////////////
+// PINES BTN/LED //
+//////////////////
+#define BTN PB5
+#define LED PB0
+
+////////////////////
+// PINES SENSORES //
+////////////////////
+#define SENSOR_1 PA6
+#define SENSOR_2 PA5
+#define SENSOR_3 PA4
+#define SENSOR_4 PA3
+#define SENSOR_5 PA2
+#define SENSOR_6 PA1
+#define SENSOR_7 PA0
+#define SENSOR_IZQUIERDO_XSLEEP PA15
+#define SENSOR_FRONTAL_XSLEEP PB3
+#define SENSOR_DERECHO_XSLEEP PB4
+
+
 void setup() {
     // put your setup code here, to run once:
 }
