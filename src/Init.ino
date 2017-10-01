25ca56453a7f7b92b6c8b11837de6fb0cdb0bee9
robotaleh <alex.santos.ete@gmail.com>
2017-10-01 12:30:30 +0200

    Añade funciones iniciales de inicialización.

--- src/Init.ino
+++ src/Init.ino
@@ -2,3 +2,33 @@
 // Este fichero contendrá todos los procesos iniciales del programa //
 // que se deberán ejecutar una sola vez al inicio del mismo.        //
 //////////////////////////////////////////////////////////////////////
+
+/**
+* Función principal de inicialización de componentes.
+*/
+void init_all(){
+  init_motors();
+  init_sensors();
+  init_led_buttons();
+}
+
+/**
+* Inicialización de los motores.
+*/
+void init_motors(){
+
+}
+
+/**
+* Inicialización de los sensores.
+*/
+void init_sensors(){
+
+}
+
+/**
+* Inicialización de leds, botones, ...
+*/
+void init_led_buttons(){
+
+}
