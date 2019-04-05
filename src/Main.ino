ee213a7d9400bb5952f073b8d9907abe6276c9db
Alex Santos <alex.santos.ete@gmail.com>
2019-04-05 17:20:25 +0200

    Elimina Wire definitivamente

--- src/Main.ino
+++ src/Main.ino
@@ -122,21 +122,6 @@
 #define MOTOR_IZQUIERDO_ENCODER_A PA15
 #define MOTOR_IZQUIERDO_ENCODER_B PB3
 
-//////////////
-// MPU9250  //
-//////////////
-#define MPU9250_ADDRESS 0x68
-
-#define GYRO_FULL_SCALE_250_DPS 0x00
-#define GYRO_FULL_SCALE_500_DPS 0x08
-#define GYRO_FULL_SCALE_1000_DPS 0x10
-#define GYRO_FULL_SCALE_2000_DPS 0x18
-
-#define ACC_FULL_SCALE_2_G 0x00
-#define ACC_FULL_SCALE_4_G 0x08
-#define ACC_FULL_SCALE_8_G 0x10
-#define ACC_FULL_SCALE_16_G 0x18
-
 //////////
 // LEDS //
 //////////
